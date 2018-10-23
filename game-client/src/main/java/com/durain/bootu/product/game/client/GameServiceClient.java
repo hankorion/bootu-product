b825dbf48d92d79488a2402450dc7fce547ca77e
hankorion <hankzhang.orion@gmail.com>
2018-10-23 16:58:02 +0800

    add sleuth before make docker

--- game-client/src/main/java/com/durain/bootu/product/game/client/GameServiceClient.java
+++ game-client/src/main/java/com/durain/bootu/product/game/client/GameServiceClient.java
@@ -3,22 +3,36 @@ package com.durain.bootu.product.game.client;
 import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.durain.bootu.product.game.client.GameServiceClient.GameClientFallback;
 import com.durain.bootu.product.game.common.GameDescreaseStockRequest;
 import com.durain.bootu.product.game.common.GameInfoResponse;
 
-@FeignClient(name = "DURAIN-GAMES")
+@FeignClient(name = "DURAIN-GAMES", fallback = GameClientFallback.class)
 public interface GameServiceClient {
 
-	@GetMapping("/games/msg")
-	String gameMsg();
-	
 	@PostMapping("/games/listFromOrder")
 	public List<GameInfoResponse> listFromOrder(@RequestBody List<String> productIdList);
-	
+
 	@PostMapping("/games/decreaseGameStock")
 	public void decreaseGameStock(@RequestBody List<GameDescreaseStockRequest> cardDTOList);
+
+	@Component
+	static class GameClientFallback implements GameServiceClient {
+
+		@Override
+		public List<GameInfoResponse> listFromOrder(List<String> productIdList) {
+			return null;
+		}
+
+		@Override
+		public void decreaseGameStock(List<GameDescreaseStockRequest> cardDTOList) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
 }
